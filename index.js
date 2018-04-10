a42ca1605f91cb2e4a2c9e69ca2a7eabac8afcb3
Leeyeonkyung <37022442+Leeyeonkyung@users.noreply.github.com>
2018-04-10 14:09:27 +0900

    a

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var app = express();
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+ var bodyParser = require('body-parser');
+ app.use(bodyParser.json()); // support json encoded bodies
+ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //=> == function() 이다. function (req,res)
+
+
   res.charset = 'UTF-8';
+  console.log(req.query);
   res.send('GET으로 넘어온 name은 '+ req.query.name + '입니다.');
 } );
+//템플릿
+app.get('/template',(req.res)=>{
+var tp1=_template(
+  '<h1>안녕 제 이름은 <%=name%>이고 나이는 <%=age%></h1>'
+);
+var obj={name: "홍길동",age:300};
+res.snd(tp1(obj) );
+});
 
-/*
 app.post('/', (req, res) => {
   res.charset = 'UTF-8';
+
+    console.log(req.query);
   res.send('POST로 넘어온 name은 '+ req.body.name + '입니다.');
 } );
-*/
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
